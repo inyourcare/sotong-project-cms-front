8fb55b3bf561bdccebd34440a98ecf5876065ab1
kwonkihoon <inyourcare@sotongfive.kr>
2022-09-26 13:51:50 +0900

    테스트 환경

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,28 +1,75 @@
 import {
     Link
 } from "react-router-dom";
-import { Button } from "@mui/material";
+import { Button, TextareaAutosize, TextField } from "@mui/material";
 import React from "react";
 
 function HomeContent() {
 
     const [state, setState] = React.useState({
+        prefix: "home_textfield_",
         apiHost: "http://localhost:8080/",
+        tableName: "",
+        columns: "",
+        databaseName: "cms_test",
     });
-    const callAPI = (apiName: string) => {
-        fetch(state.apiHost+apiName)
-            // .then(response => response.json())
-            // .then(data => console.log(data));
+    const callAPI = (apiName: string, params?: {}) => {
+        console.log(apiName, '/', params)
+        if (params) {
+            console.log('if', JSON.stringify(params))
+            fetch(state.apiHost + apiName, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(params)
+            })
+                .then(response => response.json())
+                .then(data => console.log(data));
+        } else {
+            console.log('else')
+            fetch(state.apiHost + apiName)
+                .then(response => response.json())
+                .then(data => console.log(data));
+        }
+    }
+    const handleTextFieldChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        event.preventDefault();
+        const key: string = event.target.id.replace(state.prefix, '')
+
+        setState({
+            ...state,
+            [key]: event.target.value,
+        });
     }
 
     return (
         <div>
+            <TextareaAutosize
+                maxRows={10}
+                // aria-label="maximum height"
+                // placeholder="Maximum 4 rows"
+                defaultValue=
+                {"-For Test-"+
+                "TEST_TABLE \n"
+                +"NAME VARCHAR(255),AGE INT,HEIGHT INT\n"
+                +"test"}
+                style={{ width: 500, whiteSpace: "pre-wrap"}}
+            />
             <Button
                 variant="contained"
                 onClick={() => callAPI("test")}
-                sx={{ mt: 3, ml: 1 }}
+                // sx={{ mt: 3, ml: 1 }}
             >
                 {"testCall"}
+            </Button>
+            <br />
+            <TextField id={state.prefix + "tableName"} label="Table Name" variant="filled" onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleTextFieldChange(e)} />
+            <TextField id={state.prefix + "columns"} label="Columns => ex) NAME VARCHAR(255),AGE INT,HEIGHT INT" variant="filled" onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleTextFieldChange(e)} />
+            <Button
+                variant="contained"
+                onClick={(e) => callAPI("createTable", state)}
+                // sx={{ mt: 3, ml: 1 }}
+            >
+                {"createTable"}
             </Button>
             <ul>
                 <li>
