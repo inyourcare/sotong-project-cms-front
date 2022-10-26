70f5eadbe7a38ae29036808758ae0db191e9190a
kwonkihoon <inyourcare@sotongfive.kr>
2022-10-26 15:05:17 +0900

    accessToken,refreshToken 저장

--- src/redux/reducers/app-reducer.ts
+++ src/redux/reducers/app-reducer.ts
@@ -1,5 +1,5 @@
 import { createAction, createAsyncAction, ActionType, createReducer } from "typesafe-actions";
-import { takeEvery, call } from "redux-saga/effects";
+import { takeEvery, put } from "redux-saga/effects";
 import createAsyncSaga, { createAsyncReducer, transformToArray } from "../util/reducerUtils";
 import { getAccessToken, GetAccessTokenParam } from "../../logics/auth";
 
@@ -18,20 +18,38 @@ export const signInAsyncAction = createAsyncAction(
 )<GetAccessTokenParam, Object, Error>();
 
 // sign out
-export const SIGN_OUT_ACTION = "app/SIGN_OUT";
+export const SIGN_OUT_ACTION = "app/SIGN_OUT_ACTION";
 export const signOutAction = createAction(SIGN_OUT_ACTION)<string | null>();
+
+// set tokens
+export const SET_TOKENS_ACTON = "app/SET_TOKENS_ACTON";
+export const setTokensAction = createAction(SET_TOKENS_ACTON)<SetTokensActionParam>();
 
 
 ////////////////////////////////////////////////////////////type
-const actions = { createAsyncAction, signOutAction };
+const actions = { createAsyncAction, signOutAction, setTokensAction };
 export type AppAction = ActionType<typeof actions>
 export type AppState = {
     signInAsyncAction: any,
+    accessToken: string,
+    refreshToken: string
+}
+// action params
+export type SetTokensActionParam = {
+    accessToken: string,
+    refreshToken: string,
+}
+export type SignInSuccessResult = {
+    accessToken: string,
+    grantType: string,
+    refreshToken: string
 }
 
 ////////////////////////////////////////////////////////////reducer
 const initialState: AppState = {
     signInAsyncAction: null,
+    accessToken: "",
+    refreshToken: "",
 };
 
 const app = createReducer<AppState, AppAction>(initialState)
@@ -39,18 +57,24 @@ const app = createReducer<AppState, AppAction>(initialState)
         transformToArray(signInAsyncAction),
         createAsyncReducer(signInAsyncAction, "signInAsyncAction")
     )
+    .handleAction(setTokensAction, (state, action) => {
+        return { ...state, accessToken: action.payload.accessToken, refreshToken: action.payload.refreshToken }
+    })
 
 
 ////////////////////////////////////////////////////////////saga
 export function* appSaga() {
-    yield takeEvery(SIGN_IN_ACTION,createAsyncSaga(signInAsyncAction,getAccessToken));
-    // yield takeEvery(SIGN_OUT_ACTION, signOutSaga);
+    yield takeEvery(SIGN_IN_ACTION, createAsyncSaga(signInAsyncAction, getAccessToken));
+    yield takeEvery(SIGN_IN_SUCCESS_ACTION, signInSuccessSaga);
 }
 
 // function* signInSaga(action: ReturnType<typeof signInAsyncAction.request>) {
 //     yield call(getAccessToken , action.payload)
 // }
-// function* signOutSaga(action: ReturnType<typeof signOutAction>) {
-// }
+function* signInSuccessSaga(action: ReturnType<typeof signInAsyncAction.success>) {
+    console.log('signInSuccessSaga action::', action)
+    const result = action.payload as SignInSuccessResult;
+    yield put(setTokensAction({ accessToken: result.grantType + ' ' + result.accessToken, refreshToken: result.grantType + ' ' + result.refreshToken }))
+}
 
 export default app;
